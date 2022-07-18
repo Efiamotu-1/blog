a3faa634c0ab294f8164db514e1a8d2063ee44d0
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-18 09:25:50 +0100

    add posts component

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,16 @@
+import Post from '../Post/Post'
+import './posts.css'
+
+const Posts = () => {
+  return (
+    <div className='posts'>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+    </div>
+  )
+}
+
+export default Posts
