c2b8d203e2d1762b76c34f2d4973e6bb58358356
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-19 09:53:41 +0100

    initial project commit from development branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Home from "./Pages/Home/Home";
 import TopBar from "./components/TopBar/TopBar";
 import Single from "./Pages/Single/Single";
+import Write from "./Write/Write";
 
 function App() {
   return (
     <>
      <TopBar />
      {/* <Home /> */}
-     <Single />
+     {/* <Single /> */}
+     <Write />
     </>
   );
 }
---
+++ src/Write/Write.jsx
@@ -0,0 +1,15 @@
+import './write.css'
+
+const Write = () => {
+  return (
+    <div className='write'>
+        <form className='writeform'>
+            <div className="writeFormGroup">
+              
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Write
