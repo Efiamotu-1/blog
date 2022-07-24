ab7d18fe8786f1a82ec479ec0ff5364c03fc8557
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-24 16:24:12 +0100

    add dynamic styling for topbar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './topbar.css'
 import ProfileImage from '../../assets/images/profile-img.jpg'
+import { Link } from 'react-router-dom'
 
 const TopBar = () => {
+  const user = false
   return (
     
     <header className='top'>
@@ -15,15 +17,36 @@ const TopBar = () => {
 
         <nav className="topCenter">
             <ul className='topList'>
-                <li className='topListItem'>HOME</li>
-                <li className='topListItem'>ABOUT</li>
-                <li className='topListItem'>CONTACT</li>
-                <li className='topListItem'>WRITE</li>
-                <li className='topListItem'>LOGOUT</li>
+                <li className='topListItem'>
+                  <Link to="/" className='link'>HOME</Link>   
+                </li>
+                <li className='topListItem'>
+                <Link to="/" className='link'>ABOUT</Link>     
+                </li>
+                <li className='topListItem'>
+                <Link to="/" className='link'>CONTACT</Link>   
+                </li>
+                <li className='topListItem'>
+                <Link to="/write" className='link'>WRITE</Link>   
+                  </li>
+                <li className='topListItem'>
+                <Link to="/" className='link'>{user && 'LOGOUT'}</Link>   
+                  </li>
             </ul>
         </nav>
         <div className="topRight">
-            <img src={ProfileImage} className='topImage' alt="profile" />
+          {user ? 
+          <img src={ProfileImage} className='topImage' alt="profile" />
+          :
+          <ul className='topList'>
+            <li className='topListItem'>
+          <Link to="/login" className="link">LOGIN</Link>
+            </li>
+            <li className='topListItem'>
+          <Link to="/register" className="link">REGISTER</Link>
+            </li>
+          </ul>
+        }
         <i className="topSearchIcon fas fa-search" aria-hidden="true"></i>
 
         </div>
