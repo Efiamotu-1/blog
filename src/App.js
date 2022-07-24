731e6f658d408bcd4e94c618161ef17dabc9e082
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-24 16:23:06 +0100

    add routes to each component using react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./Pages/Home/Home";
 import TopBar from "./components/TopBar/TopBar";
 import Single from "./Pages/Single/Single";
-import Write from "./Write/Write";
+import Write from "./components/Write/Write";
+import Settings from "./components/Settings/Settings";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Post from "./components/Post/Post";
 
 function App() {
+  let user = false
   return (
     <>
      <TopBar />
-     {/* <Home /> */}
-     {/* <Single /> */}
-     <Write />
+    <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/register" element={user ? <Home /> : <Register />} />
+     <Route path="/write" element={user ? <Write /> : <Register />} />
+     <Route path="/settings" element={user ? <Settings /> : <Register />} />
+     <Route path="/login" element={user ? <Home /> : <Login />} />
+     <Route path="/post/:postId" element={<Single />} />
+
+    </Routes>
     </>
   );
 }
